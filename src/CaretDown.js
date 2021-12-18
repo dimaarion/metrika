fbb610d0b3d6e16e2bda350b0402d6d8488f0fe4
dimaarion <dimaarion@gmail.com>
2021-12-18 06:55:55 +0200

    dimaarion/metrika.git

--- src/CaretDown.js
+++ src/CaretDown.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CaretDown(props) {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+		<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
   			<path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
 		</svg>
 		)
--- src/action/index.js
+++ src/action/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 let inital_base_url = 'http://adminpanel';
 if (window.location.hostname === "localhost") {
-    inital_base_url = 'http://localhost';
+    inital_base_url = 'http://fk-i-s.local';
 }else{
    inital_base_url = window.location.protocol + "//" + window.location.hostname;
    }
